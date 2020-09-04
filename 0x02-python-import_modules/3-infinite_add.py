9c7dcfa8b10f0262223f08c6d6e489ee4d85b63f
Jose Ramirez <jr047x@gmail.com>
2020-09-04 02:41:40 +0000

    test3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    import sys
+    if len(sys.argv) == 1:
+        print(0)
+    elif len(sys.argv) == 2:
+        print(sys.argv)
+    else:
+        sum = 0
+        for x in range(1, len(sys.argv)):
+            a = int(sys.argv[x])
+            sum = sum + a
+        print(sum)
