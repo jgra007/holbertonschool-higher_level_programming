7526dc5748fbf2a890c4b5445b6194af357b01e3
Jose Ramirez <jr047x@gmail.com>
2020-09-29 03:45:38 +0000

    test9.3

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,28 +1,17 @@
 #!/usr/bin/python3
-"""Simple addition
-"""
+""" Program that add 2 integers """
 
 
 def add_integer(a, b=98):
-    """Adds two variables and returns an int
-    Arguments:
-        a {int or float} -- variable containing a number
-    Keyword Arguments:
-        b {int or float} -- variable containing a number (default: {98})
-    Raises:
-        TypeError: In the case that input arguement is not an int
-        TypeError: In the case that input arguement is not an int
-    Returns:
-        int -- Result of the addition
     """
-    result = 0
-    if type(a) != float and type(a) != int:
-        raise TypeError("a must be an integer")
-    if type(b) != float and type(b) != int:
-        raise TypeError("b must be an integer")
-    if type(a) == float:
+    a and b must be integers and if its not, make a cast
+    """
+    if type(a) is int or type(a) is float:
         a = int(a)
-    if type(b) == float:
+    else:
+        raise TypeError("a must be an integer")
+    if type(b) is int or type(b) is float:
         b = int(b)
-    result = a + b
-    return result
+    else:
+        raise TypeError("b must be an integer")
+    return a + b
