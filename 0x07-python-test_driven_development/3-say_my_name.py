6a22b837f6355d38d25c486e674668f5783b4e98
Jose Ramirez <jr047x@gmail.com>
2020-09-29 04:12:18 +0000

    test11.6

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -4,10 +4,10 @@ The matrix_divided function divided matrix. For example,
 >>> matrix_divided([[1, 2, 3],[4, 5, 6]], 3)
 [[0.33, 0.67, 1.0], [1.33, 1.67, 2.0]]
 """
+
+
 def say_my_name(first_name, last_name=""):
-    """This function divided a matrix.
-    Return the result in new matrix.
-    Raise Error if data is diferrent."""
+
     if not (isinstance(first_name, str)):
         raise TypeError("first_name must be a string")
     if not (isinstance(last_name, str)):
