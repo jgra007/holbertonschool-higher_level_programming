668f1441d92e7b6beff6ed4bb4c2008e490528cc
Jose Ramirez <jr047x@gmail.com>
2020-09-29 04:33:18 +0000

    test12

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,41 +1,30 @@
 #!/usr/bin/python3
-""" Program that divide a matrix  with a x number """
+"""Function that divides all elements of a matrix"""
 
 
 def matrix_divided(matrix, div):
-    """
-    matrix its a list type with a sublist of numbers
-    """
-    horrors = {
-        "mlist": "matrix must be a matrix (list of lists) of integers/floats",
-        "rowlist": "Each row of the matrix must have the same size",
-        "div_int": "div must be a number",
-        "zero_div": "division by zero"
-    }
+    """Elements of a matrix must be int and divided by div"""
 
-    if type(matrix) is list and matrix != []:
-        if len(matrix) == 0:
-            raise TypeError(horrors["mlist"])
-        nw = []
-        row_list = None
-        for sub_list in matrix:
-            if type(sub_list) is list and sub_list != []:
-                if row_list is None:
-                    row_list = len(sub_list)
-                if row_list != len(sub_list):
-                    raise TypeError(horrors["rowlist"])
-                for elemtl in sub_list:
-                    if type(elemtl) is not int and type(elemtl) is not float:
-                        raise TypeError(horrors["mlist"])
-            else:
-                raise TypeError(horrors["mlist"])
-    else:
-        raise TypeError(horrors["mlist"])
+    e = "matrix must be a matrix (list of lists) of integers/floats"
+    if type(matrix) is not list:
+        raise TypeError(e)
+
+    size = None
+    for l in matrix:
+        if type(l) is not list:
+            raise TypeError(e)
+        if size is None:
+            size = len(l)
+        elif size != len(l):
+            raise TypeError("Each row of the matrix must have the same size")
+        for i in l:
+            if type(i) is not int and type(i) is not float:
+                raise TypeError(e)
 
     if type(div) is not int and type(div) is not float:
-        raise TypeError(horrors["div_int"])
-    if div is 0:
-        raise ZeroDivisionError(horrors["zero_div"])
+        raise TypeError("div must be a number")
 
-    nw = list(map(lambda x: list(map(lambda x: round(x / div, 2), x)), matrix))
-    return nw
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    return [[round(i / div, 2) for i in l] for l in matrix]
