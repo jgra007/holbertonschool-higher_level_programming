6ece3ee05046629e7c9a6e6dc9de81b93d976f9c
Jose Ramirez <jr047x@gmail.com>
2020-09-29 04:04:17 +0000

    test11

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""This is  the "matrix_divided" function.
-The matrix_divided function divided matrix. For example,
->>> matrix_divided([[1, 2, 3],[4, 5, 6]], 3)
-[[0.33, 0.67, 1.0], [1.33, 1.67, 2.0]]
-"""
+""" Program that print the first and last name """
+
+
 def say_my_name(first_name, last_name=""):
-    """This function divided a matrix.
-    Return the result in new matrix.
-    Raise Error if data is diferrent."""
-    if not (isinstance(first_name, str)):
+    """
+    the first and last name must be strings
+    """
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    if not (isinstance(last_name, str)):
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
+    else:
+        print("My name is {:s} {:s}".format(first_name, last_name))
