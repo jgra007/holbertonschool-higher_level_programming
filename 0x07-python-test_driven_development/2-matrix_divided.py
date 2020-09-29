1fa31ddb0407d46da9ab604a88e27080b94d0dfd
Jose Ramirez <jr047x@gmail.com>
2020-09-29 04:02:02 +0000

    test10.6

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,49 +1,41 @@
 #!/usr/bin/python3
+""" Program that divide a matrix  with a x number """
+
+
 def matrix_divided(matrix, div):
-    """''def matrix_divided(matrix, div):'' divide each
-    elelemt of the matrix is divided by div and
-    return a new matrix with the results with a 2
-    precision numbers. If div not is a number raise
-    TypeError or ZeroDivisionError if div is 0:
-    If matrix not is a list of lists return TypeError.
-    If some sub-list aren't a list return TypeError.
-    If some element of some sub-list aren't integer
-    or float number return TypeError."""
+    """
+    matrix its a list type with a sublist of numbers
+    """
+    horrors = {
+        "mlist": "matrix must be a matrix (list of lists) of integers/floats",
+        "rowlist": "Each row of the matrix must have the same size",
+        "div_int": "div must be a number",
+        "zero_div": "division by zero"
+    }
 
-    if type(div) not in [int, float]:
-        raise TypeError("div must be a number")
-    elif div is 0:
-        raise ZeroDivisionError("division by zero")
-
-    if type(matrix) is not list or len(matrix) is 0:
-        raise TypeError(
-            "matrix must be a matrix (list of lists) of \
-        integers/floats"
-        )
-    else:
-
-        for i in matrix:
-            if type(i) is not list:
-                raise TypeError(
-                    "matrix must be a matrix (list of lists) of \
-                integers/floats"
-                )
+    if type(matrix) is list and matrix != []:
+        if len(matrix) == 0:
+            raise TypeError(horrors["mlist"])
+        nw = []
+        row_list = None
+        for sub_list in matrix:
+            if type(sub_list) is list and sub_list != []:
+                if row_list is None:
+                    row_list = len(sub_list)
+                if row_list != len(sub_list):
+                    raise TypeError(horrors["rowlist"])
+                for elemtl in sub_list:
+                    if type(elemtl) is not int and type(elemtl) is not float:
+                        raise TypeError(horrors["mlist"])
             else:
-                for j in i:
-                    if type(j) not in [int, float]:
-                        raise TypeError(
-                            "matrix must be a matrix (list of lists) \
-                        of integers/floats"
-                        )
+                raise TypeError(horrors["mlist"])
+    else:
+        raise TypeError(horrors["mlist"])
 
-        r_len = len(matrix[0])
-        for n in range(len(matrix)):
-            if len(matrix[n]) != r_len:
-                raise TypeError(
-                    "Each row of the matrix must have \
-                the same size"
-                )
+    if type(div) is not int and type(div) is not float:
+        raise TypeError(horrors["div_int"])
+    if div is 0:
+        raise ZeroDivisionError(horrors["zero_div"])
 
-        New_matrix = [[round((el / div), 2) for el in i] for i in matrix]
-
-    return New_matrix
+    nw = list(map(lambda x: list(map(lambda x: round(x / div, 2), x)), matrix))
+    return nw
