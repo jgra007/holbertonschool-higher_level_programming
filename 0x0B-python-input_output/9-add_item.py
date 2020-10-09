0e39ddbbd75522ae51418c35341b38af25c45beb
Jose Ramirez <jr047x@gmail.com>
2020-10-09 16:33:25 +0000

    test11.2

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-from sys import argv
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+"""
+9. Load, add, save
+"""
+
+
+import sys
+
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-def app():
-    """adds input args to file"""
-    try:
-        a_list = load_from_json_file('./add_item.json')
-    except:
-        a_list = []
-    for i in range(1, len(argv)):
-        a_list.append(argv[i])
-    save_to_json_file(a_list, './add_item.json')
+try:
+    filej = load_from_json_file("add_item.json")
+except:
+    filej = []
 
-
-if __name__ == '__main__':
-    app()
+save_to_json_file(filej + sys.argv[1:], "add_item.json")
