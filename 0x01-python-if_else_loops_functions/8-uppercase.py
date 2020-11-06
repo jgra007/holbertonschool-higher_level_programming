306de0a8def94bff7a301377bbaf4ae0edd93ffa
Jose Ramirez <jr047x@gmail.com>
2020-11-06 19:02:03 +0000

    testx

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#1/usr/bin/python3
+def uppsercase(str):
+    for i in str:
+        i = ord(i)
+        if i >= ord(i) and i <= 122:
+            i = i - 32
+        print("{:c}".format(i), end='')
+    print()
