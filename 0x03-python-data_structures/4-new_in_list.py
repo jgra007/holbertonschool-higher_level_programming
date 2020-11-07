a7772af41c5607cde40ce5f3ef38fba3b76b33f9
Jose Ramirez <jr047x@gmail.com>
2020-11-07 22:29:48 +0000

    x

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     new_list = my_list[:]
-    
+
     for i in my_list:
         if idx < 0:
             return my_list
@@ -10,4 +10,3 @@ def new_in_list(my_list, idx, element):
         else:
             new_list[idx] = element
             return new_list
-            
