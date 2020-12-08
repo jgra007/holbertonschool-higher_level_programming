0369b9d8e18afccc17611a5fefbda1bf92ed7c93
jgra007 <jgra007@hotmail.com>
2020-12-08 17:56:26 +0000

    x

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++
@@ -1,5 +0,0 @@
--- Creates the table unique_id on your MySQL server
-CREATE TABLE IF NOT EXISTS unique_id (
-	id INT UNIQUE DEFAULT 1,
-	name VARCHAR(256)
-);
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++
@@ -1,2 +0,0 @@
--- Lists all cities contained in the database hbtn_0d_usa
-SELECT cities.id, cities.name, states.name FROM cities, states WHERE states.id = state_id;
