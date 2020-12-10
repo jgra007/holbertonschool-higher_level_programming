b2d247a5f66d3bfc6a30a9fd54d3530b2fccd77e
jgra007 <jgra007@hotmail.com>
2020-12-10 18:14:24 +0000

    x

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     print('{:d} argument{:}'.format(l - 1, '.' if l == 1 else
                                     (':' if l == 2 else 's:')))
     i = 1
-    for arg in sys.argv[1:]:
-        print("{:d}: {:s}".format(i, arg))
+    for arg in range(1, len(sys.argv)):
+        print("{:d}: {:s}".format(i, sys.argv[arg]))
         i += 1
