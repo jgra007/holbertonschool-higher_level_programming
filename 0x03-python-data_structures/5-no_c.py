1fe7c29f6dfb300c0473e3819d28eaaa3b808e97
jgra007 <jgra007@hotmail.com>
2020-12-15 18:02:32 +0000

    x

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     new_string = ''
     for x in my_string:
-        if x == 'c' or x =='C':
+        if x == 'c' or x == 'C':
             continue
         new_string += x
     return new_string
