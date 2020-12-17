70b16100f29106cbb1e3e816ddabb21ebc81753d
jgra007 <jgra007@hotmail.com>
2020-12-17 20:17:28 +0000

    x

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-
-
 def multiply_by_2(a_dictionary):
-    new_dict = {}
-    for k in a_dictionary:
-        new_dict[k] = 2 * a_dictionary[k]
-
-    return new_dict
+    return {a: 2 * x for a, x in a_dictionary.items()}
