41a55fdbb1646c288e2e70b5258b702cf7cfd919
jgra007 <jgra007@hotmail.com>
2020-12-17 18:12:13 +0000

    x

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list[:]
-
-    for i in range(len(new_list)):
-        if search == new_list[i]:
-            new_list[i] = replace
-    return new_list
+    return (list(map(lambda x: x if x != search else replace, my_list)))
