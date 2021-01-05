590763ea736879fcc124e8ec134eb1e7d35a8613
Jose <jr047x@gmail.com>
2021-01-05 21:50:06 +0000

    x

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,5 @@
-#!/usr/bin/python3
-"""
-Square Module - for all your needs, as long as they include printing squares!
-"""
+#!/usrbin/python3
 
 
 class Square:
-    """
-    First pass at creating a square class!
-    """
     pass
