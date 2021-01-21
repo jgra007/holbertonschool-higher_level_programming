fba7d80eb99f9c3c8b48f76095124b9af577b035
Jose <jr047x@gmail.com>
2021-01-21 03:37:43 +0000

    x

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''Module for task 11'''
+'''Module for task 12'''
 
 
 class Student:
@@ -11,6 +11,14 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         '''JSON representation of an instance'''
-        return self.__dict__
+        if attrs is None:
+            return self.__dict__
+
+        attrs_dict = {}
+        for att in self.__dict__:
+            if att in attrs:
+                attrs_dict[att] = self.__dict__[att]
+
+        return attrs_dict
