3d0df0c99e3d28839912c764f19e1c0e080f3620
Jose <jr047x@gmail.com>
2021-01-21 03:35:28 +0000

    x

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''Module for task 13'''
+'''Module for task 9'''
 
 
 class Student:
@@ -11,20 +11,6 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
+    def to_json(self):
         '''JSON representation of an instance'''
-        if not attrs:
-            return self.__dict__
-
-        attrs_dict = {}
-        for att in self.__dict__:
-            if att in set(attrs):
-                attrs_dict[att] = self.__dict__[att]
-
-        return attrs_dict
-
-    def reload_from_json(self, json):
-        '''Replaces all attributes by a json dict'''
-        for att in json:
-            if att in set(self.__dict__.keys()):
-                self.__dict__[att] = json[att]
+        return self.__dict__
