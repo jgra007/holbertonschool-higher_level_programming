94d2dbbd21c7dfd77d80345f12dfb522e695f4c5
= <=>
2021-05-05 19:54:37 +0000

    x

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,11 @@
-#!/usr/bin/node
-const fs = require('fs');
-const text1 = fs.readFileSync(process.argv[2], 'utf8');
-const text2 = fs.readFileSync(process.argv[3], 'utf8');
-fs.writeFileSync(process.argv[4], text1 + text2, 'utf-8');
+#!/usr/bin/nodejs
+
+// increments and calls a function.
+function addMeMaybe (number, thefunction) {
+  number += 1;
+  thefunction(number);
+}
+
+module.exports = {
+  addMeMaybe: addMeMaybe
+};
